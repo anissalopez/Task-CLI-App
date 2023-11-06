ab5f561d7235013b0fe95267919d30b900d65e8c
anissalopez <anissa965@gmail.com>
2023-11-06 11:36:51 -0500

    formatting

--- lib/helpers.py
+++ lib/helpers.py
@@ -38,9 +38,8 @@ def view_task_details(task_number):
 
 def view_by_number(choice):
     sql = """
-
         Select * from People where id is ?
-        """
+      """
 
     try:
         row = CURSOR.execute(sql, (choice,)).fetchone()
@@ -72,32 +71,34 @@ def view_by_number(choice):
     #     print("\nError retrieving task", exc)    
 
 def add_task(choice):
+
     people = Person.get_all()
     person = people[int(choice)-1]
     # person = Person.find_by_id((int(choice)-1))
     task = input("\nPlease enter task name: ")
     due_date = input("\nPlease enter task due_date in the following format 'mm-dd-yyyy': ")
     try:
-        new_task = Task.create(task, due_date, person.id)
+        Task.create(task, due_date, person.id)
         print("\nTask succesfully added")
     except Exception as exc:
         print("\nError creating new task", exc)
                  
 def manage_task(task_number, person_number):
+
     people = Person.get_all()
     person = people[int(person_number)-1]
     tasks = person.tasks()
 
-    if tasks and int(task_number) in range(1,(len(tasks)+1)):
+    if tasks and int(task_number) in range(1,(len(tasks))):
         task_action = input("\nType 'u' to update task or 'd' to delete: ")
         task = tasks[int(task_number)-1]
 
         if task_action == "u":
- 
+    
             print(f"\nTask: {task.task}, Due Date: {task.due_date}")
             updated_task = input("\nPlease enter an updated task or hit enter to leave as is: ")
             updated_duedate = input("\nPlease enter an updated due-date in the following format 'mm-dd-yyyy' or hit enter to leave as is: " )
-            
+                
             try:
                 if updated_task == "" and len(updated_duedate):
                     task.due_date = updated_duedate
@@ -110,7 +111,7 @@ def manage_task(task_number, person_number):
                 print("\nTask updated succesfully")
             except Exception as exc:
                     print("\nError creating new task ", exc)
-                      
+                        
         elif task_action == "d":   
             try:
                 task.delete()
@@ -118,9 +119,10 @@ def manage_task(task_number, person_number):
             except Exception as exc:
                 print("\nError deleting task ", exc)
         else:
-                print("\nInvalid input")
+            print("\nInvalid input")
     else:
         print("\nInvalid task number")
+      
             
 
 def find_by_person_id(personid):
